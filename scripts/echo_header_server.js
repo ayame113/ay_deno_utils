54640e5ef546a00a533b2bc4486ff85c68581488
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-03-23 17:48:07 +0900

    Update echo_header_server.js

--- scripts/echo_header_server.js
+++ scripts/echo_header_server.js
@@ -4,6 +4,14 @@ console.log("http://localhost:80/");
 for await (const req of s) {
   req.respond({
     headers: new Headers({'content-type': 'application/json'}),
-    body: JSON.stringify(Object.fromEntries(req.headers.entries()), null, 2)
+    body: JSON.stringify({
+      /*url,
+      method
+      proto
+      protoMinor
+      protoMajor*/
+      ...req
+      headers: Object.fromEntries(req.headers.entries()
+    }), null, 2)
   });
 }
