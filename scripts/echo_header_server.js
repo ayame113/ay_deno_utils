c0aa714b434b08ba8ee7afb9e854a5f79d4df736
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-03-23 16:51:33 +0900

    Update echo_header_server.js

--- scripts/echo_header_server.js
+++ scripts/echo_header_server.js
@@ -2,5 +2,7 @@ import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
 const s = serve({ port: 80 });
 console.log("http://localhost:80/");
 for await (const req of s) {
+  console.log(req.headers.entries())
+  console.log(Object.fromEntries(req.headers.entries()))
   req.respond({ body: Object.fromEntries(req.headers.entries()) });
 }
