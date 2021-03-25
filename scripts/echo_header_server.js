2f5a71e6933abe595487d7001be4be31095b178e
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-03-25 11:07:41 +0900

    Update echo_header_server.js

--- scripts/echo_header_server.js
+++ scripts/echo_header_server.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
-const s = serve({ port: 80 });
-console.log("http://localhost:80/");
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
 for await (const req of s) {
   req.respond({
     headers: new Headers({'content-type': 'application/json'}),
