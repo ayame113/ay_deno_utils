16cf10fcb417a4e7581707a4c8c6f32a40d6055f
ayame113 <ayame113@users.noreply.github.com>
2021-06-01 15:50:06 +0900

    export range function

--- scripts/range/mod.js
+++ scripts/range/mod.js
@@ -1,75 +1,177 @@
+// @ts-check
+
+/**
+ * @typedef {Object} FunctionTip
+ * @property {'map'|'filter'|'forEach'} type
+ * @property {Function} fn
+ */
+/**
+ * @template T
+ * @typedef {{done: true, value: undefined}|{done: false, value: T}} IteratorNextResult
+ */
+
+/**
+ * @template T
+ */
 class Iter {
   #from = 0;
   #to = Number.MAX_SAFE_INTEGER;
   #step = 1;
-  mapFunctions = [];
-  constructor() {
-    //
-  }
-  from(n) {
+  /** @type {Iterable<T>|undefined}*/
+  #iterable;
+  /** @type {FunctionTip[]}*/
+  #applyFunctions = [];
+  from(/** @type {Number}*/ n) {
     this.#from = n;
     return this;
   }
-  to(n) {
+  to(/** @type {Number}*/ n) {
     this.#to = n;
     return this;
   }
-  step(n) {
+  step(/** @type {Number}*/ n) {
     this.#step = n;
     return this;
   }
-  map(func) {
-    this.mapFunctions.push({
+  with(/** @type {Iterable<T>|undefined}*/ iterable) {
+    this.#iterable = iterable;
+  }
+  /**
+	* @template Tf
+	 * @param  {function(...any): Tf} fn
+	 * @return {Iter<Tf>}
+	 */
+  map(fn) {
+    this.#applyFunctions.push({
       type: "map",
-      func,
+      fn,
     });
-    return this;
+    return /** @type {*} */ (this); //Iter<T> as any as Iter<Tf>
   }
-  forEach(func) {
-    this.mapFunctions.push({
+  forEach(/** @type {Function}*/ fn) {
+    this.#applyFunctions.push({
       type: "forEach",
-      func,
+      fn,
     });
     return this;
   }
-  filter(func) {
-    this.mapFunctions.push({
+  filter(/** @type {Function}*/ fn) {
+    this.#applyFunctions.push({
       type: "filter",
-      func,
+      fn,
     });
     return this;
   }
+  /**
+	 * @return {Array<T>}
+	 */
   excute() {
-    const ignore = Symbol("ignore");
-    const result = [];
-    const step = (0 < this.#to - this.#from) === (0 < this.#step)
-      ? this.#step
-      : -this.#step;
-    console.log(this.#from, this.#to, step);
-    for (
-      let i = this.#from;
-      (i < this.#to) === (0 < this.#to - this.#from);
-      i += step
-    ) {
-      let currentValue = i;
-      for (const fn of this.mapFunctions) {
-        if (fn.type === "map") {
-          currentValue = fn.func(currentValue);
-        } else if (fn.type === "forEach") {
-          fn.func(currentValue);
-        } else if (fn.type == "filter") {
-          currentValue = fn.func(currentValue) ? currentValue : ignore;
-        }
-      }
-      if (currentValue !== ignore) {
-        result.push(currentValue);
-      }
-    }
-    return result;
+    return [...this];
+  }
+  [Symbol.iterator]() {
+    return /** @type {ReangeIterator<T>} */ (new ReangeIterator(
+      this.#from,
+      this.#to,
+      this.#step,
+      this.#iterable,
+      this.#applyFunctions,
+    ));
   }
 }
 
-console.log(
-  new Iter().to(5).from(10).step(2).map((v) => v * v).filter((v) => 99 < v)
-    .excute(),
-);
+/**
+   * @template T
+ */
+class ReangeIterator {
+  /**@type {Number}*/ #from;
+  /**@type {Number}*/ #to;
+  /**@type {Number}*/ #step;
+  /**@type {Iterable<any>|undefined}*/ #iterable;
+  /**@type {FunctionTip[]}*/ #applyFunctions;
+  /**@type {Number}*/ #i;
+  /**
+	 * @param {Number} from
+	 * @param {Number} to
+	 * @param {Number} step
+	 * @param {Iterable<any>|undefined} iterable
+	 * @param {FunctionTip[]} applyFunctions
+	 */
+  constructor(from, to, step, iterable, applyFunctions) {
+    this.#from = from;
+    this.#to = to;
+    this.#step = step;
+    this.#iterable = iterable;
+    this.#applyFunctions = applyFunctions;
+    this.#i = from;
+  }
+  /** @return {IteratorNextResult<T>} */
+  next() {
+    const done = (this.#i < this.#to) === (this.#from < this.#to);
+    if (done) {
+      return {
+        done,
+        value: void 0,
+      };
+    }
+    let isIgnored = false;
+    /**@type {any}*/
+    let value = this.#i;
+    for (const fn of this.#applyFunctions) {
+      if (fn.type === "map") {
+        value = fn.fn(value);
+      } else if (fn.type === "forEach") {
+        fn.fn(value);
+      } else if (fn.type == "filter") {
+        if (!fn.fn(value)) {
+          isIgnored = true;
+          break;
+        }
+      }
+    }
+    this.#i += (this.#from < this.#to) === (0 < this.#step)
+      ? this.#step
+      : -this.#step;
+    if (!isIgnored) {
+      return {
+        done,
+        value,
+      };
+    } else {
+      return this.next();
+    }
+  }
+}
+
+export function range(/**@type {Number[]}*/ ...args) {
+  /**@type {Iter<Number>}*/
+  const res = new Iter();
+  if (args.length === 1) {
+    res.to(args[0]);
+  } else if (args.length === 2) {
+    res.from(args[0]).to(args[1]);
+  } else {
+    res.from(args[0]).to(args[1]).step(args[3]);
+  }
+  return res;
+}
+
+if ("Deno" in globalThis) {
+  Deno.test({
+    name: "range1",
+    fn: async () => {
+      const {
+        assertEquals,
+      } = await import("https://deno.land/std@0.97.0/testing/asserts.ts");
+      assertEquals(new Iter().to(5).excute(), [0, 1, 2, 3, 4]);
+    },
+  });
+  Deno.test({
+    name: "range2",
+    fn: async () => {
+      const {
+        assertEquals,
+      } = await import("https://deno.land/std@0.97.0/testing/asserts.ts");
+      assertEquals([...new Iter().to(5)], [0, 1, 2, 3, 4]);
+    },
+  });
+}
