8f541484d61d48957a157c98616528ccc03cfe43
ayame113 <ayame113@users.noreply.github.com>
2021-06-04 12:50:59 +0900

    serialized fetch: add delay parameter

--- scripts/serialized_fetch/mod.js
+++ scripts/serialized_fetch/mod.js
@@ -1,20 +1,43 @@
 const delay = (ms) => new Promise((r) => setTimeout(r, ms));
+const isFirst = Symbol("isFirst");
 class SerializeFetcher {
-  constructor({ delay }) {
-    this.promise = new Promise();
-    this.delay = delay;
+  constructor({ delay } = {}) {
+    /**前回のリクエストが終了した時刻で解決するPromise*/
+    this.promise = Promise.resolve(isFirst);
+    this.delay = delay; //ミリ秒
   }
   fetch(...args) {
-    this.promise = this.promise.catch().then(() => delay(this.delay)).then(() =>
-      fetch(...args)
-    );
-    return this.promise;
+    const fetchPromise = this.promise.then((lastRequestEnd) => {
+      if (!this.delay) {
+        return;
+      }
+      if (lastRequestEnd === isFirst) {
+        return;
+      }
+      const elapsedTime = Date.now() - lastRequestEnd;
+      if (this.delay < elapsedTime) {
+        return;
+      }
+      return delay(this.delay - elapsedTime);
+    }).then(() => fetch(...args));
+    this.promise = fetchPromise.catch().then(() => Date.now());
+    return fetchPromise;
   }
 }
 
 export function createSerializeFetch(...args) {
   const fetcher = new SerializeFetcher(...args);
   return function fetch(...args) {
-    fetcher.fetch(...args);
+    return fetcher.fetch(...args);
   };
 }
+/*
+const start = performance.now();
+const _fetch = createSerializeFetch({ delay: 3000 });
+for (let i = 0; i < 10; i++) {
+  _fetch("https://www.yahoo.co.jp");
+  console.log(i);
+}
+await _fetch("https://www.yahoo.co.jp");
+console.log(performance.now() - start);
+*/
