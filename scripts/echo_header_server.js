ea872368e759af7fec8203284172075d9d9fe721
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-06-14 02:11:20 +0900

    fmt

--- scripts/echo_header_server.js
+++ scripts/echo_header_server.js
@@ -3,12 +3,16 @@ const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
   req.respond({
-    headers: new Headers({'content-type': 'application/json'}),
-    body: JSON.stringify({
-      url: req.url,
-      method: req.method,
-      proto: req.proto,
-      headers: Object.fromEntries(req.headers.entries())
-    }, null, 2)
+    headers: new Headers({ "content-type": "application/json" }),
+    body: JSON.stringify(
+      {
+        url: req.url,
+        method: req.method,
+        proto: req.proto,
+        headers: Object.fromEntries(req.headers.entries()),
+      },
+      null,
+      2,
+    ),
   });
 }
