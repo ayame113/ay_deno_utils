b60ce9d5be559643ad40dab14e38dcbea270ab59
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-06-15 03:45:22 +0900

    binsearch fix bug (dealing of edge values)

--- scripts/binsearch/test.ts
+++ scripts/binsearch/test.ts
@@ -12,8 +12,8 @@ Deno.test({
     });
     assertEquals(typeof a.then, "function");
     assertEquals(typeof await a, "number");
-    assertEquals(order, [50, 25, 13, 7, 4, 2, 1]);
-    assertEquals(await a, 1);
+    assertEquals(await a, 0);
+    assertEquals(order, [50, 25, 12, 6, 3, 1, 0]);
   },
 });
 
@@ -26,9 +26,9 @@ Deno.test({
       isOk: (n) => (order.push(n), Promise.resolve(false)),
     });
     assertEquals(typeof a.then, "function");
-    assertEquals(typeof await a, "number");
-    assertEquals(order, [50, 75, 88, 94, 97, 99]);
-    assertEquals(await a, 100);
+    assertEquals(typeof await a, "object");
+    assertEquals(await a, null);
+    assertEquals(order, [50, 76, 89, 95, 98, 100]);
   },
 });
 
@@ -41,8 +41,8 @@ Deno.test({
       isOk: (n) => (order.push(n), true),
     });
     assertEquals(typeof a, "number");
-    assertEquals(order, [50, 25, 13, 7, 4, 2, 1]);
-    assertEquals(a, 1);
+    assertEquals(a, 0);
+    assertEquals(order, [50, 25, 12, 6, 3, 1, 0]);
   },
 });
 
@@ -54,9 +54,9 @@ Deno.test({
       range: [0, 100],
       isOk: (n) => (order.push(n), false),
     });
-    assertEquals(typeof a, "number");
-    assertEquals(order, [50, 75, 88, 94, 97, 99]);
-    assertEquals(a, 100);
+    assertEquals(typeof a, "object");
+    assertEquals(a, null);
+    assertEquals(order, [50, 76, 89, 95, 98, 100]);
   },
 });
 
@@ -71,11 +71,11 @@ Deno.test({
     });
     assertEquals(typeof a.then, "function");
     assertEquals(typeof await a, "number");
+    assertEquals(await a, 0);
     assertEquals(
       order.map((n) => Math.round(n * 1000000) / 1000000),
-      [50, 25, 12.5, 6.3, 3.2, 1.6, 0.8, 0.4, 0.2, 0.1],
+      [50, 25, 12.5, 6.2, 3.1, 1.5, 0.7, 0.3, 0.1, 0],
     );
-    assertEquals(await a, 0.1);
   },
 });
 
@@ -89,12 +89,12 @@ Deno.test({
       digit: 1,
     });
     assertEquals(typeof a.then, "function");
-    assertEquals(typeof await a, "number");
+    assertEquals(typeof await a, "object");
+    assertEquals(await a, null);
     assertEquals(
       order.map((n) => Math.round(n * 1000000) / 1000000),
-      [50, 75, 87.5, 93.8, 96.9, 98.5, 99.3, 99.7, 99.9],
+      [50, 75.1, 87.6, 93.9, 97, 98.6, 99.4, 99.8, 100],
     );
-    assertEquals(await a, 100);
   },
 });
 
@@ -108,11 +108,11 @@ Deno.test({
       digit: 1,
     });
     assertEquals(typeof a, "number");
+    assertEquals(a, 0);
     assertEquals(
       order.map((n) => Math.round(n * 1000000) / 1000000),
-      [50, 25, 12.5, 6.3, 3.2, 1.6, 0.8, 0.4, 0.2, 0.1],
+      [50, 25, 12.5, 6.2, 3.1, 1.5, 0.7, 0.3, 0.1, 0],
     );
-    assertEquals(a, 0.1);
   },
 });
 
@@ -125,11 +125,86 @@ Deno.test({
       isOk: (n) => (order.push(n), false),
       digit: 1,
     });
-    assertEquals(typeof a, "number");
+    assertEquals(typeof a, "object");
+    assertEquals(a, null);
     assertEquals(
       order.map((n) => Math.round(n * 1000000) / 1000000),
-      [50, 75, 87.5, 93.8, 96.9, 98.5, 99.3, 99.7, 99.9],
+      [50, 75.1, 87.6, 93.9, 97, 98.6, 99.4, 99.8, 100],
     );
+  },
+});
+
+//端っこの数値だけ通す場合
+Deno.test({
+  name: "binSearch async 100<=n x10",
+  fn: async () => {
+    const order: number[] = [];
+    const a = binSearch({
+      range: [0, 100],
+      isOk: (n) => (order.push(n), Promise.resolve(100 <= n)),
+      digit: 1,
+    });
+    assertEquals(typeof a.then, "function");
+    assertEquals(typeof await a, "number");
+    assertEquals(await a, 100);
+    assertEquals(
+      order.map((n) => Math.round(n * 1000000) / 1000000),
+      [50, 75.1, 87.6, 93.9, 97, 98.6, 99.4, 99.8, 100, 99.9],
+    );
+  },
+});
+
+Deno.test({
+  name: "binSearch sync 100<=n x10",
+  fn: () => {
+    const order: number[] = [];
+    const a = binSearch({
+      range: [0, 100],
+      isOk: (n) => (order.push(n), 100 <= n),
+      digit: 1,
+    });
+    assertEquals(typeof a, "number");
     assertEquals(a, 100);
+    assertEquals(
+      order.map((n) => Math.round(n * 1000000) / 1000000),
+      [50, 75.1, 87.6, 93.9, 97, 98.6, 99.4, 99.8, 100, 99.9],
+    );
+  },
+});
+
+Deno.test({
+  name: "binSearch async 0<n x10",
+  fn: async () => {
+    const order: number[] = [];
+    const a = binSearch({
+      range: [0, 100],
+      isOk: (n) => (order.push(n), Promise.resolve(0 < n)),
+      digit: 1,
+    });
+    assertEquals(typeof a.then, "function");
+    assertEquals(typeof await a, "number");
+    assertEquals(await a, 0.1);
+    assertEquals(
+      order.map((n) => Math.round(n * 1000000) / 1000000),
+      [50, 25, 12.5, 6.2, 3.1, 1.5, 0.7, 0.3, 0.1, 0],
+    );
+  },
+});
+
+Deno.test({
+  name: "binSearch sync 0<n x10",
+  fn: () => {
+    const order: number[] = [];
+    const a = binSearch({
+      range: [0, 100],
+      isOk: (n) => (order.push(n), 0 < n),
+      digit: 1,
+    });
+    assertEquals(typeof a, "number");
+    assertEquals(a, 0.1);
+    assertEquals(
+      order.map((n) => Math.round(n * 1000000) / 1000000),
+      [50, 25, 12.5, 6.2, 3.1, 1.5, 0.7, 0.3, 0.1, 0],
+    );
   },
 });
