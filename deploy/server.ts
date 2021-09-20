29479bdf8ce014e177d2b42ec9f8e5336c747d9d
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-09-21 06:42:28 +0900

    add tsToJs

--- deploy/server.ts
+++ deploy/server.ts
@@ -1,23 +1,25 @@
-import {
-  mdToHTML,
-  serve, /*, tsToJs*/
-} from "https://deploy-gitrepo.deno.dev/v0.0.2/mod.ts";
+import { serve } from "https://deploy-gitrepo.deno.dev/v0.0.2/serve.ts";
+import { tsToJs } from "https://deploy-gitrepo.deno.dev/v0.0.2/ts_to_js.ts";
+// import { mdToHTML } from "https://deploy-gitrepo.deno.dev/v0.0.2/md_to_html.ts";
 
-const converters = [{
+const converters = [
+  /*{
   // When `match` returns true, the` convert` function is called. (The first matching converter will be used.)
   match: (request: Request) => new URL(request.url).pathname.endsWith(".md"),
   convert: mdToHTML,
-}, /*{
-  match: (request: Request) =>
-    !request.headers.get("user-agent")?.includes("Deno"),
-  convert: tsToJs,
-},*/ {
-  match: () => true,
-  convert: ({ content }: { content: string }) => ({
-    content,
-    headers: { "Access-Control-Allow-Origin": "*" },
-  }),
-}];
+}, */ {
+    match: (request: Request) =>
+      !request.headers.get("user-agent")?.includes("Deno"),
+    convert: tsToJs,
+  },
+  {
+    match: () => true,
+    convert: ({ content }: { content: string }) => ({
+      content,
+      headers: { "Access-Control-Allow-Origin": "*" },
+    }),
+  },
+];
 
 serve({
   owner: "ayame113",
